c8b1a875790d3f37babc9b5f8a135b50f155b497
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2018-05-28 16:36:33 +0800

    Removed X-Powered-By for humility. Incremented version from 0.2 to 0.3.

--- index.js
+++ index.js
@@ -484,18 +484,17 @@ var SERVER_HANDLER = function(req, res) {
     // res.end('404 not found')
     // return
     //////////////////////////////////////////
-    res.setHeader('X-Powered-By', 'XCGI')
-    //////////////////////////////////////////
     req.on('error', function(err) {
         console.log('REQUEST ERROR:', err)
     })
     //////////////////////////////////////////
     // HANDLE CORS
+    headers["Access-Control-Allow-Origin"] = "*"
     if (req.method === 'OPTIONS') {
         var headers = {}
         // IE8 does not allow domains to be specified, just the *
         // headers["Access-Control-Allow-Origin"] = req.headers.origin;
-        headers["Access-Control-Allow-Origin"] = "*"
+        // headers["Access-Control-Allow-Origin"] = "*"
         headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS"
         headers["Access-Control-Allow-Credentials"] = false
         headers["Access-Control-Max-Age"] = '86400' // 24 hours
